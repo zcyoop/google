92fae7636c07fdf5909941b6136e4743ab7e8d71
Tututu <39327876+zcyoop@users.noreply.github.com>
2023-12-07 19:54:13 +0800

    proxy githubcontent

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://github.com/";//your website url
+  let target = "https://raw.githubusercontent.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
