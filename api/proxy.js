9ff8f5b8474c2473efd2b3d76bfde0e6bf2c56c5
Tututu <39327876+zcyoop@users.noreply.github.com>
2024-09-17 16:28:11 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://twitter.com/";//your website url
+  let target = "https://rsshub.app/";//your website url
   //   if (
   //     test
   //     req.url.startsWith("/api") ||
